7cde73941d1c756f81ac696cc61ed81cda49d9e8
Dhruv chaturvedi <127045211+Dhruv9068@users.noreply.github.com>
2024-09-25 03:06:01 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,22 @@
+import AboutVisiarise from "./About_Components.jsx/AboutVisiARise";
+import FutureGoalsSection from "./About_Components.jsx/FeatureGoals";
+import OurMotiveSection from "./About_Components.jsx/OurMotive";
+import ProblemSection from "./About_Components.jsx/ProblemSection";
+import ServicesSection from "./About_Components.jsx/ServicesSection";
+import SolutionSection from "./About_Components.jsx/Solution";
+import Footer from "./Footer";
+
 const About = () => {
     return (
-      <div>
-       
-       
-      </div>
+      <>
+       <AboutVisiarise />
+        <ServicesSection />
+        <ProblemSection />
+        <SolutionSection />
+        <OurMotiveSection />
+        <FutureGoalsSection />
+        <Footer/>
+        </>
     );
   };
   
