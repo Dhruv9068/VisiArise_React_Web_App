27ce272341511e94945d1c65f26a5d4d5b37adee
Sweety Nehra <130247953+Sweetynehra@users.noreply.github.com>
2024-09-24 19:19:22 +0530

    Update painting.js

--- all_produc/painting.js
+++ all_produc/painting.js
@@ -6,22 +6,6 @@
       "category": "painting",
       "rating": 4.7,
       "review": 50
-    },
-    {
-      "imageUrl": "/images/painting2.jpg",
-      "title": "Nature Landscape Painting",
-      "price": 129,
-      "category": "painting",
-      "rating": 4.9,
-      "review": 62
-    },
-    {
-      "imageUrl": "/images/painting3.jpg",
-      "title": "Modern Canvas Art",
-      "price": 99,
-      "category": "painting",
-      "rating": 4.5,
-      "review": 35
     }
   ]
   
