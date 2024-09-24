9c8bed942630e1f61272b1b55ced25751a8505b6
Sweety Nehra <130247953+Sweetynehra@users.noreply.github.com>
2024-09-24 19:22:29 +0530

    Update cupboard.js

--- all_produc/cupboard.js
+++ all_produc/cupboard.js
@@ -6,14 +6,6 @@
       "category": "cupboard",
       "rating": 4.4,
       "review": 22
-    },
-    {
-      "imageUrl": "/images/cupboard2.jpg",
-      "title": "Modular Cupboard",
-      "price": 349,
-      "category": "cupboard",
-      "rating": 4.7,
-      "review": 30
     }
   ]
   
