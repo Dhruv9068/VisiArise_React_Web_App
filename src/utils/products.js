0db907ce537d1ac5bb1f34f71ad373789654a53a
Dhruv chaturvedi <127045211+Dhruv9068@users.noreply.github.com>
2024-09-25 18:15:51 +0530

    Add files via upload

---
+++ src/utils/products.js
@@ -0,0 +1,113 @@
+// products.js - Product Data
+const products = [
+    {
+        id: 1,
+        name: "Royal Black Table",
+        imageUrl: "/Product_img/products1.png",
+        rating: 4.5,
+        price: 29.99,
+        reviews: 245,
+        type: "Table", // Product type
+    },
+    {
+        id: 2,
+        name: "Coffee Table",
+        imageUrl: "/Product_img/products2.png",
+        rating: 4.2,
+        price: 39.99,
+        reviews: 130,
+        type: "Table", // Product type
+    },
+    {
+        id: 3,
+        name: "Classic Leather Sofa",
+        imageUrl: "/Product_img/products3.png",
+        rating: 5.0,
+        price: 49.99,
+        reviews: 310,
+        type: "Sofa", // Product type
+    },
+    {
+        id: 4,
+        name: "Fabric Recliner Sofa",
+        imageUrl: "/Product_img/products4.png",
+        rating: 5.0,
+        price: 49.99,
+        reviews: 310,
+        type: "Sofa", // Product type
+    },
+    {
+        id: 5,
+        name: "Chair Single Sofa",
+        imageUrl: "/Product_img/products5.png",
+        rating: 4.0,
+        price: 19.99,
+        reviews: 120,
+        type: "Sofa", // Product type
+    },
+    {
+        id: 6,
+        name: "Antique Dressing Table",
+        imageUrl: "/Product_img/products6.png",
+        rating: 4.8,
+        price: 69.99,
+        reviews: 500,
+        type: "Table", // Product type
+    },
+    {
+        id: 7,
+        name: "Classic Vanity Table",
+        imageUrl: "/Product_img/products7.png",
+        rating: 3.9,
+        price: 22.99,
+        reviews: 85,
+        type: "Table", // Product type
+    },
+    {
+        id: 8,
+        name: "Wooden Cupboard",
+        imageUrl: "/Product_img/products8.png",
+        rating: 4.3,
+        price: 33.99,
+        reviews: 205,
+        type: "Storage", // Product type
+    },
+    {
+        id: 9,
+        name: "Abstract Art",
+        imageUrl: "/Product_img/products9.png",
+        rating: 4.7,
+        price: 55.99,
+        reviews: 315,
+        type: "Decor", // Product type
+    },
+    {
+        id: 10,
+        name: "White Sofa",
+        imageUrl: "/Product_img/products10.png",
+        rating: 5.0,
+        price: 89.99,
+        reviews: 400,
+        type: "Sofa", // Product type
+    },
+    {
+        id: 11,
+        name: "Yellow Sofa",
+        imageUrl: "/Product_img/products11.png",
+        rating: 4.1,
+        price: 23.99,
+        reviews: 160,
+        type: "Sofa", // Product type
+    },
+    {
+        id: 12,
+        name: "Product 12",
+        imageUrl: "/Product_img/products12.png",
+        rating: 4.6,
+        price: 42.99,
+        reviews: 280,
+        type: "Misc", // Product type
+    }
+];
+
+export default products;
